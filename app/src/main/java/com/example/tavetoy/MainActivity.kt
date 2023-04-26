d94218989fec295eb6f326969879447ff908e95c
아딘컴\김아린 <jyandysys1@naver.com>
2023-04-26 18:40:26 +0900

    [FEAT] : retrofit 기본 설정

--- app/src/main/java/com/example/tavetoy/MainActivity.kt
+++ app/src/main/java/com/example/tavetoy/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.tavetoy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
+
+
+
+
