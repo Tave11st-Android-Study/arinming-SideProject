13176153462e8d1b2dbf090231a2587f346570e9
아딘컴\김아린 <jyandysys1@naver.com>
2023-04-27 19:10:50 +0900

    [FEAT] : 데이터 클래스 정의

---
+++ app/src/main/java/com/example/tavetoy/BookData.kt
@@ -0,0 +1,9 @@
+package com.example.tavetoy
+
+data class BookData(
+    val itemId: Int,
+    val title: String,
+    val author: String,
+    val publisher: String,
+    val pubDate: String
+)
---
+++ app/src/main/java/com/example/tavetoy/BookRoot.kt
@@ -0,0 +1,6 @@
+package com.example.tavetoy
+
+data class BookRoot(
+    val key: String,
+    val data: BookData
+)
