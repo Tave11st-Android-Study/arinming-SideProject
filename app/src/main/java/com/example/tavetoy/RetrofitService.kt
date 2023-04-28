6850add0b020ea3f45c86e50cf32e0264375db5e
아딘컴\김아린 <jyandysys1@naver.com>
2023-04-28 21:45:57 +0900

    [FEAT] : Retrofit Interface 선언

---
+++ app/src/main/java/com/example/tavetoy/RetrofitService.kt
@@ -0,0 +1,13 @@
+package com.example.tavetoy
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface RetrofitService {
+    @GET("search.api/item/{book}")
+    fun getBook (
+        @Path("book")
+        book: String
+    ): Call<BookRoot>
+}
