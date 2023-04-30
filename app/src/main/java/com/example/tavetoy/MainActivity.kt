94dad6699d907f6fe35ce26924a384abf638a8d4
아딘컴\김아린 <jyandysys1@naver.com>
2023-04-30 14:51:19 +0900

    [FEAT] : 네이버 API 클라이언트 ID, Secret 추가해서 테스트

--- app/src/main/java/com/example/tavetoy/MainActivity.kt
+++ app/src/main/java/com/example/tavetoy/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.tavetoy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.tavetoy.databinding.ActivityMainBinding
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -23,8 +24,6 @@ class MainActivity : AppCompatActivity() {
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    val clientId = "GBRvp2C0B4A0iYQjFL6z"
-    val clientSecret = "F0TaAoZUIT"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,8 +31,40 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-    }
+        val bookService: RetrofitService =
+            retrofitService.create(RetrofitService::class.java)
 
+        val CLIENT_ID = "GBRvp2C0B4A0iYQjFL6z"
+        val CLIENT_SECRET = "F0TaAoZUIT"
+        val callGetSearchBookData = bookService.getBookData(CLIENT_ID, CLIENT_SECRET, "테스트")
+
+        callGetSearchBookData.enqueue(object : Callback<BookRoot>{
+            override fun onResponse(call: Call<BookRoot>, response: Response<BookRoot>) {
+                if (response.isSuccessful) {
+                    Toast.makeText(this@MainActivity, "성공", Toast.LENGTH_SHORT).show()
+                    binding.textView.text = response.body()?.book?.title.toString()
+                }
+            }
+
+            override fun onFailure(call: Call<BookRoot>, t: Throwable) {
+                Log.d("로그", t.message.toString())
+            }
+        })
+        /*
+        bookService
+            .getBook("토지", 1, 1)
+            .enqueue(object: Callback<BookRoot> {
+                override fun onResponse(call: Call<BookRoot>, response: Response<BookRoot>) {
+                    if (response.isSuccessful) {
+                        binding.textView.text = response.body()?.book?.title.toString()
+                    }
+                }
+
+                override fun onFailure(call: Call<BookRoot>, t: Throwable) {
+                    Log.d("로그", t.message.toString())
+                }
+            })*/
+    }
 
 }
 
--- app/src/main/java/com/example/tavetoy/RetrofitService.kt
+++ app/src/main/java/com/example/tavetoy/RetrofitService.kt
@@ -1,14 +1,28 @@
 package com.example.tavetoy
 
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface RetrofitService {
-    @GET("v1/search/blog?query={item}")
-    fun getBook (
-        @Path("item")
-        book: String
+    @GET("v1/search/book.json")
+    fun getBookData(
+        @Header("X-Naver-Client-id")
+        clientId: String,
+        @Header("X-Naver-Client-Secret")
+        clientSecret: String,
+        @Query("item")
+        query: String,
+        @Query("display")
+        display: Int? = null,
+        @Query("start")
+        start: Int? = null
     ): Call<BookRoot>
+    fun getBook (
+
+    ): Call<BookRoot>
+    fun getBookItem (
+        @Path("item")
+        title: String
+    ): Call<BookData>
 
 }
